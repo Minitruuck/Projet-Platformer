19379688c82cc3917094517dbde19b6cb3a1dcb3
Oui <160437266+RyZzeLeMafieux@users.noreply.github.com>
2025-06-11 22:18:34 +0200

    Ajout saut sur les murs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,44 +1,49 @@
-using System.Numerics;
-using Unity.VisualScripting;
 using UnityEngine;
-// Pas besoin ?
-using UnityEngine.Android;
 
 public class PlayerMovement : MonoBehaviour
 {
-    //[SerializeField] permet de modifier la valeur de speed depuis Unity
+    //[SerializeField] -> Modifier la valeur de speed depuis Unity
+
+
+    // Vitesse de déplacement du personnage
     [SerializeField] private float speed;
+
+    // Vitesse du saut du personnage
+    [SerializeField] private float jumpPower;
+
     // Collision
     private Rigidbody2D body;
     // Animation
     private Animator anim;
 
+    // Collision
+    private BoxCollider2D boxCollider;
     // Jump
-    private bool grounded;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private LayerMask wallLayer;
+
+    // Temps d'attente pour sauter à nouveau
+    private float wallJumpCooldown;
+
+    private float horizontalInput;
+
 
     private void Awake()
     {
         // Prend les références de Rigidbody2D et d'Animator pour l'objet PlayerMovement
         body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
     // Obtenir la nouvelle position du corps dès qu'il change de place
     private void Update()
     {
         // Evite d'écrire à chaque fois Input.GetAxis()
-        float horizontalInput = Input.GetAxis("Horizontal");
+        horizontalInput = Input.GetAxis("Horizontal");
 
-        // Boutons Q et D du clavier                    // Abscisse (x) Ordonnée (y)
-        // Valeur définie par Unity compris entre -1(Q) et 1(D) <- touches par défaut sur Unity
-        body.linearVelocity = new UnityEngine.Vector2(horizontalInput * speed, body.linearVelocity.y);
-
-        // Bouton espace -> Saut
-        // Si le joueur appuie sur la barre espace et que le personnage n'est pas en l'air, il peut sauter
-        if (Input.GetKey(KeyCode.Space) && grounded)
-        {
-            Jump();
-        }
+        
+        
 
 
         // Si le personnage bouge à droite -> changement d'animation
@@ -56,25 +61,82 @@ public class PlayerMovement : MonoBehaviour
         anim.SetBool("run", horizontalInput != 0);
 
         // Animation quand le personnage saute
-        anim.SetBool("grounded", grounded);
+        anim.SetBool("grounded", isGrounded());
+
+        // Bouton espace -> Saut
+        if (wallJumpCooldown > 0.2f)
+        {
+            body.linearVelocity = new UnityEngine.Vector2(horizontalInput * speed, body.linearVelocity.y);
+
+            if (onWall() && !isGrounded())
+            {
+                // Le joueur sera collé au mur
+                body.gravityScale = 0;
+                body.linearVelocity = UnityEngine.Vector2.zero;
+            }
+            else
+            {
+                body.gravityScale = 3;
+            }
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                Jump();
+            }
+
+        }
+        else
+        {
+            wallJumpCooldown += Time.deltaTime;
+        }
     }
 
     // Fonction Jump
     private void Jump()
     {
-        // Saut                                       //x                  //y
-        body.linearVelocity = new UnityEngine.Vector2(body.linearVelocity.x, speed);
 
-        anim.SetTrigger("jump");
+        if (isGrounded())
+        {
+            // Saut                                       //x                  //y
+            body.linearVelocity = new UnityEngine.Vector2(body.linearVelocity.x, jumpPower);
+            anim.SetTrigger("jump");
+        }
+        else if (onWall() && !isGrounded())
+        {
+            if (horizontalInput == 0)
+            {
+                body.linearVelocity = new UnityEngine.Vector2(-Mathf.Sign(transform.localScale.x) * 10, 0);
+                transform.localScale = new Vector3(-Mathf.Sign(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+            }
 
-        // Le personnage n'est plus sur le sol
-        grounded = false;
+            else
+            {
+                body.linearVelocity = new UnityEngine.Vector2(-Mathf.Sign(transform.localScale.x) * 3, 6);
+            }
+            wallJumpCooldown = 0;
+        
+        }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision) {
-        if (collision.gameObject.tag == "Ground")
-        {
-            grounded = true;
-        }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+    }
+
+    private bool isGrounded()
+    {
+        // BoxCasting permet d'identifier si le joueur est sur terre même si il se trouve sur une plateforme -> Avant c'était possible que sur le sol principal
+
+        // Origine de la box, sa taille, l'angle, direction, distance, 
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, UnityEngine.Vector2.down, 0.1f, groundLayer);
+        return raycastHit.collider != null;
+    }
+    
+
+     private bool onWall()
+    {
+        // Origine de la box, sa taille, l'angle, direction, distance, 
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, new UnityEngine.Vector2(transform.localScale.x,0), 0.1f, wallLayer);
+        return raycastHit.collider != null;
     }
 }
